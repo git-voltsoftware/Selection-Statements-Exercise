6043a71e413b98c8e06c433c569eabfd76425c2c
git-volt.software <info@volt.software>
2022-12-05 19:57:32 -0500

    added selection statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+    
+            var r = new Random();
+            var favoriteNumber = r.Next(1,1000);
+
+            Console.WriteLine("Guess my favorite number.");
+            var userInput = Convert.ToInt32((Console.ReadLine()));
+
+            if ( userInput < favoriteNumber ){
+                Console.WriteLine("Too low.");
+            }else if (userInput > favoriteNumber){
+                Console.WriteLine("Too high.");
+            }else{
+                Console.WriteLine("You guessed it!");
+            }
+
+
         }
     }
 }
